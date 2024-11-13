02af068e52f91cad7c6f524c8a28585575611d69
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-11-12 22:12:48 -0300

    Adiciona exercício 2 em estruturas de controle em Python

---
+++ estruturas-de-controle/ex002.py
@@ -0,0 +1,6 @@
+# Crie uma lista com os nomes de cinco amigos e, usando um for, exiba uma mensagem de saudação para cada um.
+
+friends = ['Luis', 'Paulo', 'Caio', 'Pires', 'João Victor']
+
+for friend in friends:
+    print(f'Olá {friend}, bem-vindo a revisão de Python!')
