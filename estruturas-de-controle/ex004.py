c464734ff9b9fb692cf049f8ed2cba8c0d14d9fc
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-11-14 10:05:39 -0300

    Adiciona solução de exercício 4 de estruturas de controle em Python

---
+++ estruturas-de-controle/ex004.py
@@ -0,0 +1,17 @@
+# Crie um programa que simule uma senha de 4 dígitos. O usuário tem 3 tentativas para acertar. 
+# Use um loop while e o break para controlar as tentativas.
+
+senha = 1234
+tentativas = 3
+
+while tentativas > 0:
+    teste = int(input('Insira sua senha de acesso: '))
+    if teste != senha:
+        tentativas -= 1
+        if tentativas > 0:
+            print(f'Acesso negado. {tentativas} tentativas restantes.')
+        else:
+            print(f'Tentativas esgotadas. Bloqueando acesso ...')
+    else:
+        print('Acesso liberado.')
+        break
