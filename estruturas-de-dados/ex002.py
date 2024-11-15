484a600bc026457413b2a4064c004c02bc58c67b
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-11-15 12:46:54 -0300

    Adiciona solução em teste de alteração de elementos em ex002.py, sobre Estruturas de Dados em Python

---
+++ estruturas-de-dados/ex002.py
@@ -0,0 +1,7 @@
+# Crie uma tupla com cinco elementos e tente alterá-los para entender a imutabilidade.
+
+tupla = ('João', 'Neto', 12, 54, False)
+
+tupla[2] = 'Pedro' # Não será possível alterar item.
+
+print(tupla[2]) 
