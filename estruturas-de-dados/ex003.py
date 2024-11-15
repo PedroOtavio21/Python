ac3f4c8c4e5fa5de8e17d18e6d4359d0931a2a9a
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-11-15 12:47:47 -0300

    Adiciona solução de teste em exibição de dados repetidos em ex003.py, sobre estruturas de dados em python

---
+++ estruturas-de-dados/ex003.py
@@ -0,0 +1,7 @@
+# Crie um conjunto a partir de uma lista que contém elementos duplicados e mostre como os duplicados são removidos.
+
+pares = {2,4,6,8,10,12,2,4,4,10} 
+
+for element in pares:
+    print(element) # Ao printar, os valores repetidos serão "Removidos"
+
