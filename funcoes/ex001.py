3ef4e712f8328484cdce7a85ed088501a4002515
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-11-15 13:14:38 -0300

    Adiciona solução de exercícios 1 e 2 em Funções em Python

---
+++ funcoes/ex001.py
@@ -0,0 +1,9 @@
+# Crie uma função que receba dois números como parâmetros e retorne a soma deles. 
+# Em seguida, chame a função com diferentes valores.
+
+def sum(x, y):
+    return x + y
+
+print(f"Soma 1: {sum(3,5)}")
+print(f'Soma 2: {sum(5, -15)}')
+print(f'Soma 3: {sum(55,-40)}')
---
+++ funcoes/ex002.py
@@ -0,0 +1,11 @@
+# Crie uma função que receba uma lista de números e retorne apenas os números pares utilizando a função filter().
+
+def pares(lista):
+    pares = filter(lambda x: x % 2 == 0, lista)
+    return list(pares)
+
+numeros = [1,4,3,45,2,6,7,8]
+
+nova_lista = pares(numeros)
+
+print(nova_lista)
